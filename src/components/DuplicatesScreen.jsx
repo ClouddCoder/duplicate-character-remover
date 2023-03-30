3552b23c3caa25fd18ab42db70a707ec35026f6e
Brayan Sanchez <brayan.sanchez.leon@correounivalle.edu.co>
2023-03-30 14:05:02 -0500

    Create the two screens

---
+++ src/components/DuplicatesScreen.jsx
@@ -0,0 +1,14 @@
+function DuplicatesScreen({ characters }) {
+  return (
+    <div>
+      <h2>Remove the duplicates</h2>
+      <div>
+        {characters.map((char, index) => (
+          <div key={index}>{char}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default DuplicatesScreen;
---
+++ src/components/InputScreen.jsx
@@ -0,0 +1,11 @@
+function InputScreen() {
+  return (
+    <div id="user-input-container">
+      <form id="user-input-form">
+        <label htmlFor="string">Put the string</label>
+        <input id="string" type="text" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
