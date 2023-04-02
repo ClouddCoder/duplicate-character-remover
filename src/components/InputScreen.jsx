32e1e148fcf595e5bb37e048bb98826e288f7aa5
Brayan Sanchez <brayan.sanchez.leon@correounivalle.edu.co>
2023-04-01 20:17:57 -0500

    Install bootstrap and change style of the input Screen

--- src/components/InputScreen.jsx
+++ src/components/InputScreen.jsx
@@ -1,10 +1,25 @@
 function InputScreen({ onHandleSubmit, onString, onHandleChange }) {
   return (
     <div id="user-input-container">
-      <form id="user-input-form" onSubmit={onHandleSubmit}>
-        <label htmlFor="string">Put the string</label>
-        <input id="string" type="text" value={onString} onChange={onHandleChange} />
-        <button>Submit</button>
+      <form className="row gy-2 gx-3 align-items-center" onSubmit={onHandleSubmit}>
+        <div className="col-auto">
+          <label className="visually-hidden" htmlFor="autoSizingInput">
+            String
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="autoSizingInput"
+            placeholder="String"
+            value={onString}
+            onChange={onHandleChange}
+          />
+        </div>
+        <div className="col-auto">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
       </form>
     </div>
   );
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import { useEffect } from "react";
+import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    typeof document !== undefined ? require("bootstrap/dist/js/bootstrap") : null;
+  }, []);
+
+  return <Component {...pageProps} />;
 }
